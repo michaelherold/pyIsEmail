5ff21eff0e8e177a888f5fb1dbd90276cfaf6e2f
Michael Herold <michael.j.herold@gmail.com>
2013-10-29 21:48:33 -0400

    Declare support for Python 3.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
         "Topic :: Communications :: Email",
         "Topic :: Software Development :: Libraries",
